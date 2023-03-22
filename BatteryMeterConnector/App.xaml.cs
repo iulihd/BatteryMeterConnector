9f097c5d54767af57d8197d129bd5becbd5c6fdc
Iulian Maftei <38617475+iulihd@users.noreply.github.com>
2023-03-22 11:09:04 -0400

    Add project files.

---
+++ BatteryMeterConnector/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿// Author: Iulian Maftei
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BatteryMeterConnector
+{
+    public partial class App : Application
+    {
+    }
+}
